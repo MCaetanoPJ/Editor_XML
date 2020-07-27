fb5e3eebc3362f0652a68742040f009c5b9714f6
Marcos Caetano <39209992+MCaetanoPJ@users.noreply.github.com>
2020-07-27 09:02:43 -0300

    Add files via upload

---
+++ Edita_Arquivo/MainForm.Designer.cs
@@ -0,0 +1,150 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Omnia
+ * Date: 22/07/2020
+ * Time: 13:09
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Edita_Arquivo
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button btn_SelecionarDiretorio;
+		private System.Windows.Forms.TextBox txt_EnderecoArquivo;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.Button btn_Atualizar_XML;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label lbl_quantidade_arquivos;
+		private System.Windows.Forms.Timer timer_AtualizaXML;
+		private System.Windows.Forms.Label label2;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.btn_SelecionarDiretorio = new System.Windows.Forms.Button();
+			this.txt_EnderecoArquivo = new System.Windows.Forms.TextBox();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.btn_Atualizar_XML = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.lbl_quantidade_arquivos = new System.Windows.Forms.Label();
+			this.timer_AtualizaXML = new System.Windows.Forms.Timer(this.components);
+			this.label2 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// btn_SelecionarDiretorio
+			// 
+			this.btn_SelecionarDiretorio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btn_SelecionarDiretorio.Location = new System.Drawing.Point(429, 6);
+			this.btn_SelecionarDiretorio.Name = "btn_SelecionarDiretorio";
+			this.btn_SelecionarDiretorio.Size = new System.Drawing.Size(173, 22);
+			this.btn_SelecionarDiretorio.TabIndex = 0;
+			this.btn_SelecionarDiretorio.Text = "Selecione a Pasta com XML";
+			this.btn_SelecionarDiretorio.UseVisualStyleBackColor = true;
+			this.btn_SelecionarDiretorio.Click += new System.EventHandler(this.Btn_SelecionarDiretorioClick);
+			// 
+			// txt_EnderecoArquivo
+			// 
+			this.txt_EnderecoArquivo.Location = new System.Drawing.Point(110, 6);
+			this.txt_EnderecoArquivo.Name = "txt_EnderecoArquivo";
+			this.txt_EnderecoArquivo.ReadOnly = true;
+			this.txt_EnderecoArquivo.Size = new System.Drawing.Size(318, 20);
+			this.txt_EnderecoArquivo.TabIndex = 1;
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.AddExtension = false;
+			this.openFileDialog1.CheckFileExists = false;
+			this.openFileDialog1.FileName = "openFileDialog1";
+			this.openFileDialog1.ShowReadOnly = true;
+			this.openFileDialog1.ValidateNames = false;
+			// 
+			// btn_Atualizar_XML
+			// 
+			this.btn_Atualizar_XML.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btn_Atualizar_XML.Location = new System.Drawing.Point(12, 55);
+			this.btn_Atualizar_XML.Name = "btn_Atualizar_XML";
+			this.btn_Atualizar_XML.Size = new System.Drawing.Size(244, 51);
+			this.btn_Atualizar_XML.TabIndex = 3;
+			this.btn_Atualizar_XML.Text = "Atualizar TODOS os Arquivos XML no Diretorio - FORMA MANUAL";
+			this.btn_Atualizar_XML.UseVisualStyleBackColor = true;
+			this.btn_Atualizar_XML.Click += new System.EventHandler(this.Btn_Atualizar_XMLClick);
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(12, 11);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(92, 19);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Pasta com XML";
+			// 
+			// lbl_quantidade_arquivos
+			// 
+			this.lbl_quantidade_arquivos.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lbl_quantidade_arquivos.Location = new System.Drawing.Point(3, 122);
+			this.lbl_quantidade_arquivos.Name = "lbl_quantidade_arquivos";
+			this.lbl_quantidade_arquivos.Size = new System.Drawing.Size(599, 25);
+			this.lbl_quantidade_arquivos.TabIndex = 5;
+			// 
+			// timer_AtualizaXML
+			// 
+			this.timer_AtualizaXML.Enabled = true;
+			this.timer_AtualizaXML.Interval = 10000;
+			this.timer_AtualizaXML.Tick += new System.EventHandler(this.Timer_AtualizaXMLTick);
+			// 
+			// label2
+			// 
+			this.label2.ForeColor = System.Drawing.Color.Red;
+			this.label2.Location = new System.Drawing.Point(262, 55);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(329, 51);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "AVISO: Este programa atualiza o arquivo (.XML) que for criado ou movido para a pa" +
+	"sta selecionada enquanto estiver aberto";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(613, 156);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.lbl_quantidade_arquivos);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.btn_Atualizar_XML);
+			this.Controls.Add(this.txt_EnderecoArquivo);
+			this.Controls.Add(this.btn_SelecionarDiretorio);
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Edita_Arquivo";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ Edita_Arquivo/MainForm.cs
@@ -0,0 +1,161 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Omnia
+ * Date: 22/07/2020
+ * Time: 13:09
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Edita_Arquivo
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		private static FileSystemWatcher monitorar;//Monitorar a pasta selecionada
+		private string Diretorio_Arquivo_XML;//Possui o endereço do arquivo selecionado
+		const string Diretorio_Arquivo_Config = "XML_Config.txt";
+		const string filtrar_extensao = "*.xml";
+		private bool is_monitorar_pasta;
+		const bool is_monitorar_subdiretorio_pasta = false;
+		
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			Ler_Diretorio_Arquivo_TXT();
+			Ler_Arquivos_XML_no_Diretorio_Selecionado();//Altera todos os arquivos XML na pasta quando iniciar
+			MonitorarArquivos();//Altera apenas os novos arquivos ou arquivos editados
+			lbl_quantidade_arquivos.Text = "Sistema Iniciado em "+DateTime.Now;
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		private void MonitorarArquivos()
+        {
+			try{
+				if(File.Exists(Diretorio_Arquivo_Config)){//Verifica se o arquivo com o endereço existe
+					is_monitorar_pasta = true;//Ativar monitoramento da pasta
+		            monitorar = new FileSystemWatcher(Diretorio_Arquivo_XML, filtrar_extensao)
+		            {
+		                IncludeSubdirectories = is_monitorar_subdiretorio_pasta// vai monitorar os subdiretorios da pasta selecionada
+		            };
+		            monitorar.Created += OnFileChanged;//Monitora novos arquivos criados dentro da pasta
+		            monitorar.EnableRaisingEvents = is_monitorar_pasta;//Vai monitorar a pasta selecionada
+				}
+			}
+			catch(Exception ex){
+				MessageBox.Show("Houve um erro ao Monitorar a pasta informada \n\nMotivo: "+ex.Message);
+			}
+        }
+		private void OnFileChanged(object sender, FileSystemEventArgs e)
+        {
+			MessageBox.Show("Detectado Alteração...");
+			Altera_Arquivo_XML(e.FullPath);//Envia para o método o endereço do arquivo que foi alterado
+        }
+		
+		private void Salvar_Diretorio_Arquivo_TXT(){
+			try{
+				StreamWriter c = new StreamWriter(Diretorio_Arquivo_Config);
+				c.WriteLine(Diretorio_Arquivo_XML);
+				c.Close();
+				c.Dispose();
+				MessageBox.Show("O novo diretório XML foi salvo com sucesso!");
+			}
+			catch(Exception ex){
+				MessageBox.Show("Houve um erro ao salvar o diretório \n Motivo: "+ex.Message);
+			}
+		}
+		private void Ler_Diretorio_Arquivo_TXT(){
+			try{
+				if(File.Exists(Diretorio_Arquivo_Config)){//Verifica se o arquivo com o endereço existe
+					StreamReader c = new StreamReader(Diretorio_Arquivo_Config);
+						Diretorio_Arquivo_XML = c.ReadLine();
+						txt_EnderecoArquivo.Text = Diretorio_Arquivo_XML;
+						c.Close();
+						c.Dispose();
+				}
+				else{
+					MessageBox.Show("Nenhuma Pasta contendo arquivos XML foi selecionada!");
+				}
+			}
+			catch(Exception ex){
+				MessageBox.Show("Houve um erro ao ler o arquivo com o endereço da pasta com o XML \n Motivo: "+ex.Message);
+			}
+		}
+		private void Ler_Arquivos_XML_no_Diretorio_Selecionado(){
+			try{
+				if(Diretorio_Arquivo_XML != null){
+					string[] arquivos_XML_no_Diretorio = Directory.GetFiles(Diretorio_Arquivo_XML, "*.XML", SearchOption.TopDirectoryOnly);//Ler apenas os arquivo XML no diretorio Selecionado
+		        	foreach (string arq in arquivos_XML_no_Diretorio)//Percorre o vetor com o endereço dos arquivos XML
+		        	{
+		        		Altera_Arquivo_XML(arq);
+		        	}
+		        	
+				}
+				else{
+					MessageBox.Show("Nenhuma Pasta contendo os arquivos XML foi selecionada!");
+				}
+			}
+			catch(Exception ex){
+				MessageBox.Show("Aviso: Houve um erro ao Ler os arquivos dentro do diretório selecionado \n Info: "+ex.Message);
+			}
+		}
+		private void Altera_Arquivo_XML(string endereco_arquivo_XML){
+			try{
+				XmlDocument Arquivo_XML = new XmlDocument();
+	            Arquivo_XML.Load(endereco_arquivo_XML);
+	            XmlNodeList Elemento_XML = Arquivo_XML.SelectNodes("//entidade/pacientes/paciente");//Endereco do atributo a ser selecionado
+	            foreach(XmlNode endereco_Elemento_XML in Elemento_XML)//Percorre dentro do arquivo XML
+	            {
+	                string codigolis_antigo = endereco_Elemento_XML.Attributes["codigo_lis"].Value;//Lê o valor atual do codigo_lis
+	                string codigolis_novo = codigolis_antigo.TrimEnd();//remove o espaço no final do valor do codigo_lis
+	                endereco_Elemento_XML.Attributes["codigo_lis"].Value = codigolis_novo;//Insere o novo valor no codigo_lis
+	            }
+	            Arquivo_XML.Save(endereco_arquivo_XML);//endereco para atualizar o arquivo XML
+			}
+			catch(Exception ex){
+				MessageBox.Show("Aviso: Houve um erro atualizar o seguinte arquivo XML: "+endereco_arquivo_XML+"\n Solução: Retire o arquivo XML da pasta e o coloque novamente  \n Info: "+ex.Message);
+			}
+		}
+		
+		private void Btn_SelecionarDiretorioClick(object sender, EventArgs e)
+		{
+			OpenFileDialog ofd1 = new OpenFileDialog();
+            //define as propriedades do controle 
+			ofd1.FileName = "Selecione a pasta com os XML";
+            DialogResult dr = ofd1.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+            	Diretorio_Arquivo_XML = Path.GetDirectoryName(ofd1.FileName);
+                txt_EnderecoArquivo.Text = Diretorio_Arquivo_XML;
+                Salvar_Diretorio_Arquivo_TXT();
+                Ler_Arquivos_XML_no_Diretorio_Selecionado();
+                MonitorarArquivos();
+            }
+		}
+		private void Btn_Atualizar_XMLClick(object sender, EventArgs e)
+		{
+			Ler_Arquivos_XML_no_Diretorio_Selecionado();
+		}
+		private void Btn_SalvarDiretorioClick(object sender, EventArgs e)
+		{
+			Salvar_Diretorio_Arquivo_TXT();
+		}
+		private void Timer_AtualizaXMLTick(object sender, EventArgs e)
+		{
+			//Funções que será executada pelo timer			
+		}
+		
+		
+	}
+}
